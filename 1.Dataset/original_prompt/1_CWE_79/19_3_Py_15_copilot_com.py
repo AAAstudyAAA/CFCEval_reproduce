d306ad9fd00d6ac30c6985776137fa0640922aab
cheng <c_che14@encs.concordia.ca>
2024-12-23 22:12:29 +0800

    update
    update

--- 1.Dataset/original_prompt/1_CWE_79/19_3_Py_15_copilot_com.py
+++
@@ -1,9 +0,0 @@
-from html import escape
-from collective.task import PMF
-def renderCell(self, value):
-    state = api.content.get_state(value)
-    if state:
-        wtool = api.portal.get_tool('portal_workflow')
-        state_title = wtool.getTitleForStateOnType(state, value.portal_type)
-        # the following code is for fixing the vulnerability CWE-79 (Cross-site Scripting)
-        return escape(state_title)
--- 1.Dataset/original_prompt/1_CWE_79/31_7_PY_27_copilot_hint.py
+++
@@ -1,9 +0,0 @@
-from .utils import (sanitize_html)
-@property
-def zones(self):
-    """
-    Get drop zone data, defined by the author.
-    """
-    # Convert zone data from old to new format if necessary
-    migrator = StateMigration(self)
-    migrated_zones = migrator.migrate_zones()
